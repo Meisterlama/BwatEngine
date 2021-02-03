3877323c38be0cce7ba6bd86fb91d6f6a9593b7d
Meisterlama <56450272+Meisterlama@users.noreply.github.com>
2021-02-03 16:51:56 +0100

    Added Random related functions (#17)

---
+++ Math/include/Misc/Misc.hpp
@@ -0,0 +1,7 @@
+#ifndef MATH_MISC_HPP
+#define MATH_MISC_HPP
+
+#include "Noise.hpp"
+#include "RNG.hpp"
+
+#endif //MATH_MISC_HPP
---
+++ Math/include/Misc/RNG.hpp
@@ -0,0 +1,33 @@
+#ifndef MATH_RNG_HPP
+#define MATH_RNG_HPP
+
+#include "Meta.hpp"
+
+namespace Math
+{
+    class RNG {
+    public:
+        RNG(unsigned int seed = 0) : m_seed(seed), m_position(0) {};
+
+        // State modifiers
+        ML_FUNC_DECL void ResetSeed(unsigned int seed, int position = 0) {m_seed = seed; m_position = position;}
+        [[nodiscard]] ML_FUNC_DECL unsigned int GetSeed() const { return m_seed; }
+        ML_FUNC_DECL void SetCurrentPosition(int position) { m_position = position;}
+        [[nodiscard]] ML_FUNC_DECL int GetCurrentPosition() const {return m_position;}
+
+        // RNGs
+        [[nodiscard]] ML_FUNC_DECL unsigned int RollRandomUint32();
+        [[nodiscard]] ML_FUNC_DECL unsigned short RollRandomUint16();
+        [[nodiscard]] ML_FUNC_DECL unsigned char RollRandomByte();
+        [[nodiscard]] ML_FUNC_DECL unsigned int RollRandomIntLessThan(unsigned int maxValue);
+        [[nodiscard]] ML_FUNC_DECL int RollRandomIntInRange(int minValue, int maxValue);
+        [[nodiscard]] ML_FUNC_DECL float RollRandomFloatZeroToOne();
+        [[nodiscard]] ML_FUNC_DECL float RollRandomFloatInRange(float minValue, float maxValue);
+        [[nodiscard]] ML_FUNC_DECL bool RollRandomChance(float probabilityOfReturningTrue);
+
+    private:
+        unsigned int m_seed;
+        int m_position;
+    };
+}
+#endif //MATH_RNG_HPP
