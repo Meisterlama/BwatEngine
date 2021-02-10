0e82fc3cc28dbb4d7cffae217588b0437ff445e4
Baptiste SUZON <b.suzon@student.isartdigital.com>
2021-02-10 10:07:55 +0100

    Reworked math architecture, removing inl files and added pragma regions

--- Engine/include/Math/Misc/RNG.hpp
+++ Engine/include/Math/Misc/RNG.hpp
@@ -5,6 +5,9 @@
 
 namespace BMath
 {
+
+#pragma region Declarations
+
     class RNG {
     public:
         RNG(unsigned int seed = 0) : m_seed(seed), m_position(0) {};
@@ -30,6 +33,44 @@ namespace BMath
         int m_position;
     };
 
-    #include "inl/RNG.inl.hpp"
+#pragma endregion
+
+#pragma region Definitions
+
+    ML_FUNC_DECL unsigned int RNG::RollRandomUint32()
+    {
+        return Get1dNoiseUint(m_position++, m_seed);
+    }
+    ML_FUNC_DECL unsigned short RNG::RollRandomUint16()
+    {
+        return static_cast<unsigned short>(Get1dNoiseUint(m_position++, m_seed));
+    }
+    ML_FUNC_DECL unsigned char RNG::RollRandomByte()
+    {
+        return static_cast<unsigned char>(Get1dNoiseUint(m_position++, m_seed));
+    }
+    ML_FUNC_DECL unsigned int RNG::RollRandomIntLessThan(unsigned int maxValue)
+    {
+        return static_cast<unsigned int>(Get1dNoiseZeroToOne(m_position++, m_seed) * maxValue);
+    }
+    ML_FUNC_DECL int RNG::RollRandomIntInRange(int minValue, int maxValue)
+    {
+        return minValue + static_cast<unsigned int>(Get1dNoiseZeroToOne(m_position++, m_seed) * maxValue);
+    }
+    ML_FUNC_DECL float RNG::RollRandomFloatZeroToOne()
+    {
+        return Get1dNoiseZeroToOne(m_position++, m_seed);
+    }
+    ML_FUNC_DECL float RNG::RollRandomFloatInRange(float minValue, float maxValue)
+    {
+        return minValue * Get1dNoiseZeroToOne(m_position++, m_seed) * maxValue;
+    }
+    ML_FUNC_DECL bool RNG::RollRandomChance(float probabilityOfReturningTrue)
+    {
+        return (Get1dNoiseZeroToOne(m_position++, m_seed) < probabilityOfReturningTrue) ? true : false;
+    }
+
+#pragma endregion
+
 }
 #endif //MATH_RNG_HPP
