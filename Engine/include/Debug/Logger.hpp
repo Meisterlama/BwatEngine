5dd1e3b06afb998e65657273b9ce4d4ed9f394d8
Hahn william <w.hahn@student.isartdigital.com>
2021-02-17 10:08:47 +0100

    remove LoggerSingleton.hpp, add the flag for the debug states on macros

--- Engine/include/Debug/Logger.hpp
+++ Engine/include/Debug/Logger.hpp
@@ -2,13 +2,7 @@
 #define DEBUG_LOGGER_HPP
 
 
-//for the first one
-#include <fstream>
-#include <string>
-#include <mutex>
 
-
-// for the snd one
 #include <cstdio>
 #include <cstdarg>
 #include <cstdbool>
@@ -16,52 +10,6 @@
 
 namespace BLogger
 {
-#if 0
-    //the first log try
-
-    class Log
-    {
-    public:
-        // Constructor with Parameter
-        Log(std::string fileName);
-
-        // disable the copy for the constructor and assignment
-        Log(const Log&) = delete;
-        Log& operator=(const Log&) = delete;
-
-        // create a move for the constructor and assignment
-        Log(Log&& other)
-        {
-            myStream.close();
-            myStream = move(other.myStream);
-        }
-
-        Log& operator=(Log&& other)
-        {
-            myStream.close();
-            myStream = move(other.myStream);
-
-            return *this;
-        }
-
-
-        // Destructor
-        ~Log()
-        {
-         myStream.close();
-        }
-
-        // Write in the logFile
-        void WriteLine(std::string content);
-
-    private:
-        std::ofstream myStream;
-        std::mutex myMutex;
-    };
-
-#else
-    //the second logger class that can be used
-
     typedef struct {
         va_list ap;
         const char *fmt;
@@ -77,12 +25,25 @@ namespace BLogger
 
     enum { E_LOG_TRACE, E_LOG_DEBUG, E_LOG_INFO, E_LOG_WARN, E_LOG_ERROR, E_LOG_FATAL };
 
+#ifdef DEBUG
+
 #define LogTrace(...) LogLog(BLogger::E_LOG_TRACE, __FILE__, __LINE__, __VA_ARGS__ )
-#define LogDebug(...) LogLog(BLogger::E_LOG_DEBUG, __FILE__, __LINE__, _VA_ARGS_ )
+#define LogDebug(...) LogLog(BLogger::E_LOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__ )
 #define LogInfo(...) LogLog(BLogger::E_LOG_INFO, __FILE__, __LINE__, __VA_ARGS__ )
 #define LogWarn(...) LogLog(BLogger::E_LOG_WARN, __FILE__, __LINE__, __VA_ARGS__ )
 #define LogError(...) LogLog(BLogger::E_LOG_ERROR, __FILE__, __LINE__, __VA_ARGS__ )
 #define LogFatal(...) LogLog(BLogger::E_LOG_FATAL, __FILE__, __LINE__, __VA_ARGS__ )
+
+#else
+
+#define LogTrace(...) LogNull()
+#define LogDebug(...) LogNull()
+#define LogInfo(...) LogNull()
+#define LogWarn(...) LogNull()
+#define LogError(...) LogNull()
+#define LogFatal(...) LogNull()
+
+#endif
 
     const char* LogLevelString(int level);
     void LogSetLock(LockFn fn, void *uData);
@@ -93,8 +54,9 @@ namespace BLogger
 
     void LogLog(int level, const char*file, int line, const char *fmt, ...);
 
+    void LogNull();
 
-#endif
+
 }
 
 
--- Engine/src/Debug/Logger.cpp
+++ Engine/src/Debug/Logger.cpp
@@ -1,36 +1,10 @@
 #include "Debug/Logger.hpp"
 
-//#include <ctime>
 #include <cstdio>
 
 namespace BLogger
 {
-#if 0
-    Log::Log(std::string fileName)
-    {
-        myStream.open(fileName);
-
-        if (myStream.fail())
-            throw std::iostream::failure("Cannot open file: " + fileName);
-    }
-
-    void Log::WriteLine(std::string content)
-    {
-        std::lock_guard<std::mutex> lock(myMutex);
-
-        std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-        char timestamp[26];
-        ctime_s(timestamp, sizeof timestamp, &now);
-
-        std::string timestampNoEndl(timestamp);
-        timestampNoEndl = timestampNoEndl.substr(0,24);
-
-        myStream << timestampNoEndl << ": " << content << std::endl;
-
-    }
-#else
-
-#define MAX_CALLBACKS 32
+    #define MAX_CALLBACKS 32
 
 typedef struct {
         LogFn fn;
@@ -174,8 +148,8 @@ static const char *LevelColors[] = {
         unlock();
     }
 
+    void LogNull()
+    {
 
-
-
-#endif
+    }
 }
