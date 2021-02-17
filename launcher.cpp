8346c9bf5ab1cf5eb4c6f6864aa6bd635b5abeb3
Theo Ritouni <t.ritouni@student.isartdigital.com>
2021-02-17 16:20:13 +0100

    quick Fix : move main.cpp to launcher.cpp

--- launcher.cpp
+++ launcher.cpp
@@ -1,91 +1,25 @@
-#include <iostream>
-#include <cstdio>
-
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
-#ifdef BWATEDITOR
-
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
 
-// flag for the debug option allow to use logger if define.
-//#define DEBUG
+//#include <assimp/Importer.hpp>      // C++ importer interface
+//#include <assimp/scene.h>           // Output data structure
+//#include <assimp/postprocess.h>     // Post processing flags
 
-#include "Debug/Logger.hpp"
-
-#endif //BWATEDITOR
-
+#define BMATH_IMPLEMENTATION
 #include "Math/Math.hpp"
 
-#include <assimp/Importer.hpp>      // C++ importer interface
-#include <assimp/scene.h>           // Output data structure
-#include <assimp/postprocess.h>     // Post processing flags
+#include "Triangle.hpp"
+#include "Window.hpp"
+#include "Rendering/shader.hpp"
+#include "Rendering/model.hpp"
+#include "Rendering/camera.hpp"
 
-
-typedef const char* (*glGetStringFunc)(int);
-
-extern "C"
-{
-	//DLLEXPORT int NvOptimusEnablement = 1;
-	//DLLEXPORT int AmdPowerXpressRequestHighPerformance = 1;
-}
-
-void debugGLCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam)
-{
-	printf("GL error = %s\n", message);
-}
-
-void debugGLFWCallback(int error_code, const char* description)
-{
-	printf("GLFW error (%x) = %s\n", error_code, description);
-}
-
-GLFWwindow* InitGLFW()
-{
-	glfwInit();
-
-	glfwSetErrorCallback(debugGLFWCallback);
-
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	GLFWwindow* window = glfwCreateWindow(800, 600, "TestEngine", nullptr, nullptr);
-	glfwMakeContextCurrent(window);
-
-	return window;
-}
-
-void InitGlad()
-{
-	gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress));
-
-	std::cout << " RENDERER = " << glGetString(GL_RENDERER) << std::endl;
-	std::cout << " VENDOR = " << glGetString(GL_VENDOR) << std::endl;
-	std::cout << " VERSION = " << glGetString(GL_VERSION) << std::endl;
-
-	// Setup KHR_debug callback
-	if (GLAD_GL_KHR_debug)
-	{
-		glEnable(GL_DEBUG_OUTPUT);
-		glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
-		glDebugMessageCallback((GLDEBUGPROC)debugGLCallback, nullptr);
-		glDebugMessageControl(GL_DONT_CARE, GL_DEBUG_TYPE_PERFORMANCE, GL_DONT_CARE, 0, nullptr, GL_FALSE);
-		glDebugMessageControl(GL_DONT_CARE, GL_DEBUG_TYPE_OTHER, GL_DONT_CARE, 0, nullptr, GL_FALSE);
-	}
-}
 
 int main()
 {
-	// init glfw
-	GLFWwindow* window = InitGLFW();
+	Bwat::Window mainWindow;
 
-	// init glad
-	InitGlad();
-#ifdef BWATEDITOR
 	// Init ImGui
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
@@ -94,27 +28,32 @@ int main()
 	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
 	ImGui::StyleColorsDark();
 
-	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplGlfw_InitForOpenGL(mainWindow.window, true);
 	ImGui_ImplOpenGL3_Init("#version 330");
 #endif
 
-	float color[3] = { 0.8f, 0.2f, 0.2f };
+	// Shader
+	Rendering::Shader myShader("basic.vs", "basic.fs");
+	Rendering::Model myModel((std::string)"media/bag/backpack.obj");
 
+	// time init var
+	float deltaTime = 0.0f;
+	float lastFrame = 0.0f;
 
-	FILE *file;
-	errno_t err;
-	err = fopen_s(&file,"Log.txt", "w");
+	// Camera init
+	Rendering::Camera cam;
 
-#ifdef BWATEDITOR
-    BLogger::LogAddFp(file, 0);
-    BLogger::LogDebug("test1");
-#endif //BWATEDITOR
+	float color[3] = { 0.5f, 0.2f, 0.5f };
+	Rendering::Triangle myTri;
 
-	while (!glfwWindowShouldClose(window))
+	while (mainWindow.IsWorking())
 	{
-		glfwPollEvents();
+		//Time
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
 
-#ifdef BWATEDITOR
+		//ImGui
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
@@ -122,23 +61,42 @@ int main()
 		ImGui::ColorEdit3("Clear color", color);
 #endif
 
+		// Depth Test and buffer
+		glEnable(GL_DEPTH_TEST);
+		glViewport(0, 0, mainWindow.GetWidth(), mainWindow.GetHeight());
 		glClearColor(color[0], color[1], color[2], 1.f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		//Camera 
+		cam.usefreefly(&mainWindow, deltaTime);
+
+		// projection, view and model 
+		BMath::Matrix4<float> projection = BMath::Matrix4<float>::CreatePerspective(60.f, mainWindow.GetWidth() / mainWindow.GetHeight(), 0.1f, 100.0f);
+		BMath::Matrix4<float> view = cam.ViewOfCamera();
+		BMath::Matrix4<float> model = BMath::Matrix4<float>::CreateScaleMat(1.f);
+
+		//use shader 
+		glUseProgram(myShader.ID);
+		myShader.setMat4("proj", projection);
+		myShader.setMat4("model", model);
+		myShader.setMat4("view", view);
+
+		//myTri.Update();
+		myModel.Draw(myShader);
 
 #ifdef BWATEDITOR
 		ImGui::Render();
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-#endif
-		glfwSwapBuffers(window);
+
+		glfwSwapBuffers(mainWindow.window);
+		glfwPollEvents();
 	}
 #ifdef BWATEDITOR
 	ImGui_ImplGlfw_Shutdown();
 	ImGui_ImplOpenGL3_Shutdown();
 	ImGui::DestroyContext();
-#endif //BWATEDITOR
 
-	glfwDestroyWindow(window);
-	glfwTerminate();
+	mainWindow.Close();
 
 	return 0;
 }
