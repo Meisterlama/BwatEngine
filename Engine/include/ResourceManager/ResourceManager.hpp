819418f89b9af68ffaddb0b13ffb9864244adbe6
Hahn william <w.hahn@student.isartdigital.com>
2021-03-19 16:01:14 +0100

    no need the ResourceManager.cpp anymore everything work inside the .Hpp

--- Engine/include/ResourceManager/ResourceManager.hpp
+++ Engine/include/ResourceManager/ResourceManager.hpp
@@ -11,6 +11,10 @@ public:
 
     static ResourceManager* Instance()
     {
+        static ResourceManager* instance = nullptr;
+        if (instance == nullptr)
+            instance = new ResourceManager;
+
         return instance;
     }
 
@@ -20,7 +24,7 @@ public:
 
     /* constructors */
 
-    ResourceManager();
+
 
     ResourceManager(const ResourceManager&) = delete;
     ResourceManager operator=(const ResourceManager) = delete;
@@ -30,37 +34,73 @@ public:
     /* All the load resources with template to call and stock on the good map */
 
     template<typename T>
-    void LoadResource(const char* path);
-/*
-    template<>
-    void LoadResource<SoundResource>(const char* path);
-*/
-    template<>
-    void LoadResource<Rendering::Model>(const char* path);
+    void LoadResource(const char* path) {}
+
+    //template<>
+    //void LoadResource<SoundResource>(const char* path);
 
     template<>
-    void LoadResource<Rendering::Texture>(const char* path);
+    void LoadResource<Rendering::Model>(const char* path)
+    {
+        if (!path)
+            return;
+
+        std::string str(path);
+        Rendering::Model toLoad ( str );
+        models.emplace(path, toLoad);
+    }
+
+    template<>
+    void LoadResource<Rendering::Texture>(const char* path)
+    {
+        if (!path)
+            return;
+
+        Rendering::Texture toLoad;
+        //toLoad.TextureFromFile(path);
+        textures.emplace(path, toLoad);
+    }
 
     /* ************************************************************************* */
 
     /* get the resources with the template type */
 
     template<typename T>
-    T* GetResource(const char* path);
-/*
-    template<>
-    T* GetResource<SoundResource>(const char* path);
-*/
-    template<>
-    Rendering::Model* GetResource<Rendering::Model>(const char* path);
+    T* GetResource(const char* path) {}
+
+    /*template<>
+    //T* GetResource<SoundResource>(const char* path)
+    {
+        if (sounds.find(path) != sounds.cend())
+            return sounds[path];
+
+        return nullptr;
+    }
+    */
 
     template<>
-    Rendering::Texture* GetResource<Rendering::Texture>(const char* path);
+    Rendering::Model* GetResource<Rendering::Model>(const char* path)
+    {
+        if (models.find(path) != models.cend())
+            return &models[path];
+
+        return nullptr;
+    }
+
+    template<>
+    Rendering::Texture* GetResource<Rendering::Texture>(const char* path)
+    {
+        if (textures.find(path) != textures.cend())
+            return &textures[path];
+
+        return nullptr;
+    }
 
     /* ************************************************************************* */
 
 private:
 
+    ResourceManager() = default;
     static ResourceManager* instance;
 
     /* all the maps that stores the resources */
