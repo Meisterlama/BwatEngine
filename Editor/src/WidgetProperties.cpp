6becbde294e7796cb6a92f030f32df30fe0cd294
Meisterlama <56450272+Meisterlama@users.noreply.github.com>
2021-04-01 15:27:45 +0200

    Added possibility to create entities in editor (#106)

--- Editor/src/WidgetProperties.cpp
+++ Editor/src/WidgetProperties.cpp
@@ -1,6 +1,7 @@
 #include <ECS/Components/TransformComponent.hpp>
 #include <ECS/Components/RenderableComponent.hpp>
 #include <ECS/Components/RigidBodyComponent.hpp>
+#include <ECS/Components/AudioSourceComponent.hpp>
 
 #include "ECS/Coordinator.hpp"
 
@@ -47,27 +48,95 @@ void WidgetProperties::ShowComponent<BwatEngine::RigidBodyComponent>(BwatEngine:
     }
 }
 
+template<>
+void WidgetProperties::ShowComponent<BwatEngine::AudioSourceComponent>(BwatEngine::AudioSourceComponent &component)
+{
+    if (ImGui::CollapsingHeader("Audio Source", ImGuiTreeNodeFlags_DefaultOpen))
+    {
+        bool update = false;
+        float gain = component.source.GetGain();
+        float pitch = component.source.GetPitch();
+        bool loop = component.source.GetLooping();
+
+        update |= ImGui::DragFloat("gain", &gain, 0.1f, 0.0f, 100.f);
+        update |= ImGui::DragFloat("pitch", &pitch, 0.01f, 0.0f, 100.f);
+        update |= ImGui::Checkbox("loop", &loop);
+
+        if (update)
+        {
+            component.source.SetGain(gain);
+            component.source.SetPitch(pitch);
+            component.source.SetLooping(loop);
+        }
+
+        if(ImGui::Button("Play"))
+            component.source.Play();
+    }
+}
+
 void WidgetProperties::TickVisible()
 {
     using namespace BwatEngine;
 
-        Coordinator& coordinator = *Coordinator::GetInstance();
-        Signature entitySignature = coordinator.GetEntitySignature(currentEntity);
+    Coordinator &coordinator = *Coordinator::GetInstance();
+    if (ImGui::Button("Create Entity"))
+    {
+        coordinator.CreateEntity();
+    }
 
-        if (entitySignature.test(coordinator.GetComponentType<TransformComponent>()))
-            ShowComponent(coordinator.GetComponent<TransformComponent>(currentEntity));
+    Signature entitySignature = coordinator.GetEntitySignature(currentEntity);
 
-        if (entitySignature.test(coordinator.GetComponentType<RenderableComponent>()))
-            ShowComponent(coordinator.GetComponent<RenderableComponent>(currentEntity));
+    if (ImGui::BeginMenu("AddComponent"))
+    {
+        bool hasComponentAvailable = false;
 
-        if (entitySignature.test(coordinator.GetComponentType<RigidBodyComponent>()))
-            ShowComponent(coordinator.GetComponent<RigidBodyComponent>(currentEntity));
+        hasComponentAvailable |= AddComponentMenuItem<TransformComponent>(currentEntity);
+        hasComponentAvailable |= AddComponentMenuItem<RigidBodyComponent>(currentEntity);
+        hasComponentAvailable |= AddComponentMenuItem<RenderableComponent>(currentEntity);
+        hasComponentAvailable |= AddComponentMenuItem<AudioSourceComponent>(currentEntity);
+
+        ImGui::EndMenu();
+    }
+
+    ShowComponentMenuItem<TransformComponent>(currentEntity);
+    ShowComponentMenuItem<RenderableComponent>(currentEntity);
+    ShowComponentMenuItem<RigidBodyComponent>(currentEntity);
+    ShowComponentMenuItem<AudioSourceComponent>(currentEntity);
 }
 
-void WidgetProperties::Inspect(BwatEngine::Entity& entity)
+void WidgetProperties::Inspect(BwatEngine::Entity &entity)
 {
     currentEntity = entity;
 }
 
+template<typename T>
+bool WidgetProperties::AddComponentMenuItem(BwatEngine::Entity entity)
+{
+    BwatEngine::Coordinator &coordinator = *BwatEngine::Coordinator::GetInstance();
+    BwatEngine::Signature entitySignature = coordinator.GetEntitySignature(entity);
+
+    if (!entitySignature.test(coordinator.GetComponentType<T>()))
+    {
+        //TODO: proper component name
+        if (ImGui::MenuItem(typeid(T).name()))
+            //TODO: proper default value for the component
+            coordinator.AddComponent<T>(entity, {});
+        return true;
+    }
+    return false;
+}
 
 
+template<typename T>
+bool WidgetProperties::ShowComponentMenuItem(BwatEngine::Entity entity)
+{
+    BwatEngine::Coordinator &coordinator = *BwatEngine::Coordinator::GetInstance();
+    BwatEngine::Signature entitySignature = coordinator.GetEntitySignature(entity);
+
+    if (entitySignature.test(coordinator.GetComponentType<T>()))
+    {
+        ShowComponent(coordinator.GetComponent<T>(currentEntity));
+        return true;
+    }
+    return false;
+}
