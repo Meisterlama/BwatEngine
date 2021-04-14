52747b2e33f2fce24095b0b5109520d90fa08a40
w.hahn <w.hahn@student.isartdigital.com>
2021-04-14 13:53:14 +0200

    Fix serialization probleme caused by ECS rework
    fix the serialization error caused when trying to save data, because ECS format as changed.

---
+++ Engine/include/Serialization/SerializeComponent.hpp
@@ -0,0 +1,55 @@
+#ifndef ENGINE_SERIALIZECOMPONENT_HPP
+#define ENGINE_SERIALIZECOMPONENT_HPP
+
+using json = nlohmann::json;
+using ordered_json = nlohmann::ordered_json;
+
+namespace BwatEngine
+{
+
+    typedef TransformComponent;
+namespace Serializable
+{
+    template<typename T>
+    void SaveComponent(T, json& js)
+    {
+    }
+
+    template<>
+    void
+    SaveComponent<BwatEngine::TransformComponent>(TransformComponent transform, json& js)
+    {
+        js +=
+                json{
+                        {"transform",
+                                {
+                                        {"position",
+                                                {
+                                                        {"X", transform.position.X},
+                                                        {"Y", transform.position.Y},
+                                                        {"Z", transform.position.Z}
+                                                },
+                                        },
+                                        {"rotation",
+                                                {
+                                                        {"X", transform.rotation.X},
+                                                        {"Y", transform.rotation.Y},
+                                                        {"Z", transform.rotation.Z}
+                                                },
+                                        },
+                                        {"scale",
+                                                {
+                                                        {"X", transform.scale.X},
+                                                        {"Y", transform.scale.Y},
+                                                        {"Z", transform.scale.Z}
+                                                }
+                                        }
+                                }
+                        }
+                };
+
+    };
+};
+};
+
+#endif
--- Engine/src/Serialization/Serialization.cpp
+++ Engine/src/Serialization/Serialization.cpp
@@ -8,80 +8,56 @@
 #include "iomanip"
 #include "Debug/Logger.hpp"
 
-
-using json = nlohmann::json;
-using ordered_json = nlohmann::ordered_json;
+#include "Serialization/SerializeComponent.hpp"
 
 namespace BwatEngine {
 
-    Serializer::Serializer(const char *path)
-            : path(path) {
-
-    }
-
-
-    void Serializer::SaveScene(const BwatEngine::Scene &toSave) {
+    void Serializer::SaveScene(const BwatEngine::Scene &toSave, const char* path)
+    {
         std::ofstream file(path);
 
         json js;
-        Coordinator &coordinator = *Coordinator::GetInstance();
+        Coordinator &coordinator = Coordinator::GetInstance();
 
 
         for (int i = 0; i < toSave.entities.size(); i++)
         {
             TransformComponent transform = coordinator.GetComponent<TransformComponent>(toSave.entities[i]);
-            js["Entity"] +=
-                    json{
-                            {"transform",
-                                    {
-                                            {"position",
-                                                {
-                                                        {"X", transform.transform.position.X},
-                                                        {"Y", transform.transform.position.Y},
-                                                        {"Z", transform.transform.position.Z}
-                                                },
-                                            },
-                                            {"rotation",
-                                                {
-                                                    {"X", transform.transform.rotation.X},
-                                                    {"Y", transform.transform.rotation.Y},
-                                                    {"Z", transform.transform.rotation.Z}
-                                                },
-                                            },
-                                            {"scale",
-                                                {
-                                                    {"X", transform.transform.scale.X},
-                                                    {"Y", transform.transform.scale.Y},
-                                                    {"Z", transform.transform.scale.Z}
-                                                }
-                                            }
-                                    }
-                            }
-                    };
-       }
+            Serializable::SaveComponent<TransformComponent>(transform, js["Entity"]);
 
 
-            file << std::setw(4) << js << std::endl;
         }
 
-        void Serializer::LoadData(Scene &toLoad) {
+            file << std::setw(4) << js << std::endl;
+    }
+
+        void Serializer::LoadScene(Scene &toLoad, const char* path)
+        {
             std::ifstream file(path);
 
+            if (!file)
+            {
+                LogError("No File at path :%s", path);
+                return;
+            }
 
             json js;
             file >> js;
 
-            Coordinator &coordinator = *Coordinator::GetInstance();
+            Coordinator &coordinator = Coordinator::GetInstance();
 
 
             for (int i = 0; i < toLoad.entities.size(); i++) {
 
-                json transform = js["Entity"][i]["transform"];
+                json transform  = js["Entity"][i]["transform"];
+                json position   = transform.at("position");
+                json rotation   = transform.at("rotation");
+                json scale      = transform.at("scale");
 
                 coordinator.GetComponent<TransformComponent>(toLoad.entities[i]) = {Math::Transform{
-                        Math::Vec3f{transform.at("position").at("X").get<float>(), transform.at("position").at("Y").get<float>(),transform.at("position").at("Z").get<float>()},
-                        Math::Vec3f{transform.at("rotation").at("X").get<float>(),transform.at("rotation").at("Y").get<float>(),transform.at("rotation").at("Z").get<float>()},
-                        Math::Vec3f{transform.at("scale").at("X").get<float>(), transform.at("scale").at("Y").get<float>(), transform.at("scale").at("Z").get<float>()}
+                        Math::Vec3f{position.at("X").get<float>(), position.at("Y").get<float>(),position.at("Z").get<float>()},
+                        Math::Vec3f{rotation.at("X").get<float>(),rotation.at("Y").get<float>(),rotation.at("Z").get<float>()},
+                        Math::Vec3f{scale.at("X").get<float>(), scale.at("Y").get<float>(), scale.at("Z").get<float>()}
                 }};
             }
 
@@ -89,6 +65,9 @@ namespace BwatEngine {
 // all the component + data
 
 // rigid body save as : exist
+
+
+//
 
 
 }
