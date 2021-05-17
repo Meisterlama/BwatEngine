1e8c7b387687c48c773435b004ef585357f9757b
Hahn William <55888138+lorions4@users.noreply.github.com>
2021-05-17 10:32:49 +0200

    Engine/serialization (#155)
    * Add the script and light component in the serialization
    
    now the script and the light component are save and loaded inside the serialization
    
    * Add the name component to the serialization

--- Engine/src/Serialization/Serialization.cpp
+++ Engine/src/Serialization/Serialization.cpp
@@ -30,6 +30,9 @@ namespace BwatEngine {
             Serializable::SaveComponent<RigidBodyComponent>(currentEntity, js["Entity"][i]);
             Serializable::SaveComponent<RenderableComponent>(currentEntity, js["Entity"][i]);
             Serializable::SaveComponent<ColliderComponent>(currentEntity, js["Entity"][i]);
+            Serializable::SaveComponent<ScriptComponent>(currentEntity, js["Entity"][i]);
+            Serializable::SaveComponent<LightComponent>(currentEntity, js["Entity"][i]);
+            Serializable::SaveComponent<DataComponent>(currentEntity, js["Entity"][i]);
         }
 
             file << std::setw(4) << js << std::endl;
@@ -74,7 +77,12 @@ namespace BwatEngine {
                        Serializable::Load<RenderableComponent>(newEntity, componentData);
                    else if (componentId == "collider")
                        Serializable::Load<ColliderComponent>(newEntity, componentData);
-
+                   else if (componentId == "script")
+                       Serializable::Load<ScriptComponent>(newEntity, componentData);
+                   else if (componentId == "light")
+                       Serializable::Load<LightComponent>(newEntity, componentData);
+                   else if (componentId == "data")
+                       Serializable::Load<DataComponent>(newEntity, componentData);
 
                }
 
