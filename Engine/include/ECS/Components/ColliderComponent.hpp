ad7c8ff242ef6578d42c2bf33ec115bb8af41115
Baptiste SUZON <baptiste_suzon@hotmail.com>
2021-05-17 14:15:36 +0200

    ECS fix to prevent premature resource deletion

--- Engine/include/ECS/Components/ColliderComponent.hpp
+++ Engine/include/ECS/Components/ColliderComponent.hpp
@@ -8,7 +8,28 @@ namespace BwatEngine
 	struct ColliderComponent
 	{
 		Collider* collider = nullptr;
+
+		ColliderComponent(Math::Vec3f scale = {1.0f}) // Cube
+        {
+            collider = new BoxCollider{scale};
+        }
+
+        ColliderComponent(float radius) // Sphere
+        {
+            collider = new SphereCollider{radius};
+        }
+
+        ~ColliderComponent()
+        {
+            if (collider)
+            {
+                delete collider;
+                collider = nullptr;
+            }
+        }
 	};
+
+
 }
 
 #endif // !COLLIDERCOMPONENT_HPP
