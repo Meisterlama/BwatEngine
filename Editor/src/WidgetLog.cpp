ae0dc30a3d22eeee0d5b3712b1af3e21277da8a9
Meisterlama <56450272+Meisterlama@users.noreply.github.com>
2021-05-21 15:06:10 +0200

    fix the size problem with the debug window (#192)
    Co-authored-by: Hahn william <w.hahn@student.isartdigital.com>

--- Editor/src/WidgetLog.cpp
+++ Editor/src/WidgetLog.cpp
@@ -18,14 +18,21 @@ WidgetLog::~WidgetLog()
     BLogger::LogRemoveCallback(callbackIndex);
 }
 
+#include <vector>
 void WidgetLog::OnLogCallback(BLogger::LogEvent* ev)
 {
-    char buffer[1024];
-    vsprintf(buffer, ev->fmt, ev->ap);
-    ss << buffer << "\n";
+    static std::vector<char> buffer; // Avoid allocation for each call
+    int size = vsnprintf(nullptr, 0, ev->fmt, ev->ap);
+    if (buffer.size() < size)
+        buffer.resize(size+1);
+
+    vsnprintf(buffer.data(), buffer.size(), ev->fmt, ev->ap);
+    ss << buffer.data() << "\n";
+    std::string yolo = ss.str();
 }
 
 void WidgetLog::TickVisible()
 {
     ImGui::TextWrapped(ss.str().c_str());
+    //printf("%s\n\n", ss.str().c_str());
 }
